072b2afbb577be4d195301e38045bad63d7aeebf
Nischal Khadka <justchalchal@gmail.com>
2022-12-05 12:34:00 +0545

    Final Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*html'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'poppins' : ['Poppins', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
